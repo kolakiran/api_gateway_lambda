a0ef92994ffd7ba8050fc14f6400245f219828d0
kolakiran <kolakiranbabu@gmail.com>
2024-03-08 11:18:01 -0500

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
     console.log("Event: ", event);
     return {
         statusCode: 200,
-        body: JSON.stringify({ message: "Hello World from Lambda!" }),
+        body: JSON.stringify({ message: "Hello World from lambda" }),
     };
 };
